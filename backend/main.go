6002f9ff361ab3995eb778ac7259bdaafd488bdb
Dongjue Yang <87690744+yangdongjue5510@users.noreply.github.com>
2025-08-03 01:57:39 +0900

    feat: Thing 입력 및 조회 구현

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"yangdongju/gtd-todo/thing"
-	
+	"github.com/gin-contrib/cors"
+	"time"
 )
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // Allow all origins for simplicity))
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"Content-Length"},
+		MaxAge:           12 * time.Hour,
+	}))
 	thingRoutes(r) // Setup the thing routes
 	r.Run(":8080") // Start the server on port 8080
 }
